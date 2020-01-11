fd2f579b77f0b6dcde139bedd0f472672ba297e4
Soc Sieng <socsieng@gmail.com>
2020-01-11 03:17:45 -0800

    Fix issue with version filter plugin

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ import { VersionFilterPlugin } from './version-filter-plugin';
 module.exports = (PluginHost: Application) => {
   const app = PluginHost.owner;
 
-  app.renderer.addComponent('typescript-declaration', new TypeScriptDeclarationPlugin(app.renderer))
-    .applyConfiguration();
   app.converter.addComponent('version-filter', new VersionFilterPlugin(app.converter));
   app.converter.addComponent('keyof-comment', new KeyOfCommentPlugin(app.converter));
+
+  const declarationPlugin = app.renderer.addComponent('typescript-declaration', new TypeScriptDeclarationPlugin(app.renderer));
+
+  declarationPlugin.applyConfiguration();
 }
