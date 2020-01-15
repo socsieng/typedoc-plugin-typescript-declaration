4d52efcdcec8f96f2e1f984591c2fd3ddbf1d9f2
Soc Sieng <socsieng@gmail.com>
2020-01-15 00:59:17 -0800

    Refactor renderers into a separate file

--- src/render/reflection-formatter.ts
+++ src/render/reflection-formatter.ts
@@ -1,23 +1,5 @@
-import { Reflection, ReflectionKind } from 'typedoc/dist/lib/models';
-import AccessorRenderer from './accessor-renderer';
-import CallSignatureRenderer from './call-signature-renderer';
-import ConstructorSignatureRenderer from './constructor-signature-renderer';
-import ContainerRenderer from './container-renderer';
-import EnumMemberRenderer from './enum-member-renderer';
-import EventRenderer from './event-renderer';
-import FunctionRenderer from './function-renderer';
-import GetSignatureRenderer from './get-signature-renderer';
-import IndexSignatureRenderer from './index-signature-renderer';
-import MethodRenderer from './method-renderer';
-import ObjectLiteralRenderer from './object-literal-renderer';
-import ParameterRenderer from './parameter-renderer';
-import ProjectRenderer from './project-renderer';
-import PropertyRenderer from './property-renderer';
-import ReflectionRenderer from './reflection-renderer';
-import SetSignatureRenderer from './set-signature-renderer';
-import TypeAliasRenderer from './type-alias-renderer';
-import TypeLiteralRenderer from './type-literal-renderer';
-import TypeParameterRenderer from './type-parameter-renderer';
+import { Reflection } from 'typedoc/dist/lib/models';
+import { renderers } from './renderers';
 
 /**
  * Sort flags
@@ -44,38 +26,9 @@ export const sortMapping: { [key: string]: ReflectionSortFlags | undefined } = {
 export default class ReflectionFormatter {
   public static sortOption: ReflectionSortFlags = ReflectionSortFlags.none;
 
-  private _renderers: { [kind: number]: ReflectionRenderer } = {};
-
-  constructor() {
-    this._renderers[ReflectionKind.Global] = new ProjectRenderer();
-    this._renderers[ReflectionKind.ExternalModule] = new ContainerRenderer('module');
-    this._renderers[ReflectionKind.Module] = new ContainerRenderer('module');
-    this._renderers[ReflectionKind.Class] = new ContainerRenderer('class');
-    this._renderers[ReflectionKind.Enum] = new ContainerRenderer('enum');
-    this._renderers[ReflectionKind.Interface] = new ContainerRenderer('interface');
-    this._renderers[ReflectionKind.Method] = new MethodRenderer();
-    this._renderers[ReflectionKind.Constructor] = new MethodRenderer();
-    this._renderers[ReflectionKind.CallSignature] = new CallSignatureRenderer();
-    this._renderers[ReflectionKind.GetSignature] = new GetSignatureRenderer();
-    this._renderers[ReflectionKind.SetSignature] = new SetSignatureRenderer();
-    this._renderers[ReflectionKind.ConstructorSignature] = new ConstructorSignatureRenderer();
-    this._renderers[ReflectionKind.Accessor] = new AccessorRenderer();
-    this._renderers[ReflectionKind.Property] = new PropertyRenderer();
-    this._renderers[ReflectionKind.Variable] = new PropertyRenderer();
-    this._renderers[ReflectionKind.EnumMember] = new EnumMemberRenderer();
-    this._renderers[ReflectionKind.TypeLiteral] = new TypeLiteralRenderer();
-    this._renderers[ReflectionKind.TypeAlias] = new TypeAliasRenderer();
-    this._renderers[ReflectionKind.Function] = new FunctionRenderer();
-    this._renderers[ReflectionKind.Parameter] = new ParameterRenderer();
-    this._renderers[ReflectionKind.TypeParameter] = new TypeParameterRenderer();
-    this._renderers[ReflectionKind.Event] = new EventRenderer();
-    this._renderers[ReflectionKind.IndexSignature] = new IndexSignatureRenderer();
-    this._renderers[ReflectionKind.ObjectLiteral] = new ObjectLiteralRenderer();
-  }
-
   public render(reflection?: Reflection, terminatorCharater?: string): string {
     if (reflection) {
-      const renderer = this._renderers[reflection.kind];
+      const renderer = renderers[reflection.kind];
 
       if (renderer) {
         return renderer.render(reflection, terminatorCharater);
---
+++ src/render/renderers.ts
@@ -0,0 +1,47 @@
+import AccessorRenderer from './accessor-renderer';
+import CallSignatureRenderer from './call-signature-renderer';
+import ConstructorSignatureRenderer from './constructor-signature-renderer';
+import ContainerRenderer from './container-renderer';
+import EnumMemberRenderer from './enum-member-renderer';
+import EventRenderer from './event-renderer';
+import FunctionRenderer from './function-renderer';
+import GetSignatureRenderer from './get-signature-renderer';
+import IndexSignatureRenderer from './index-signature-renderer';
+import MethodRenderer from './method-renderer';
+import ObjectLiteralRenderer from './object-literal-renderer';
+import ParameterRenderer from './parameter-renderer';
+import ProjectRenderer from './project-renderer';
+import PropertyRenderer from './property-renderer';
+import { ReflectionKind } from 'typedoc/dist/lib/models';
+import ReflectionRenderer from './reflection-renderer';
+import SetSignatureRenderer from './set-signature-renderer';
+import TypeAliasRenderer from './type-alias-renderer';
+import TypeLiteralRenderer from './type-literal-renderer';
+import TypeParameterRenderer from './type-parameter-renderer';
+
+export const renderers: { [kind: number]: ReflectionRenderer } = {
+  [ReflectionKind.Accessor]: new AccessorRenderer(),
+  [ReflectionKind.CallSignature]: new CallSignatureRenderer(),
+  [ReflectionKind.Class]: new ContainerRenderer('class'),
+  [ReflectionKind.Constructor]: new MethodRenderer(),
+  [ReflectionKind.ConstructorSignature]: new ConstructorSignatureRenderer(),
+  [ReflectionKind.Enum]: new ContainerRenderer('enum'),
+  [ReflectionKind.EnumMember]: new EnumMemberRenderer(),
+  [ReflectionKind.Event]: new EventRenderer(),
+  [ReflectionKind.ExternalModule]: new ContainerRenderer('module'),
+  [ReflectionKind.Function]: new FunctionRenderer(),
+  [ReflectionKind.GetSignature]: new GetSignatureRenderer(),
+  [ReflectionKind.Global]: new ProjectRenderer(),
+  [ReflectionKind.IndexSignature]: new IndexSignatureRenderer(),
+  [ReflectionKind.Interface]: new ContainerRenderer('interface'),
+  [ReflectionKind.Method]: new MethodRenderer(),
+  [ReflectionKind.Module]: new ContainerRenderer('module'),
+  [ReflectionKind.ObjectLiteral]: new ObjectLiteralRenderer(),
+  [ReflectionKind.Parameter]: new ParameterRenderer(),
+  [ReflectionKind.Property]: new PropertyRenderer(),
+  [ReflectionKind.SetSignature]: new SetSignatureRenderer(),
+  [ReflectionKind.TypeAlias]: new TypeAliasRenderer(),
+  [ReflectionKind.TypeLiteral]: new TypeLiteralRenderer(),
+  [ReflectionKind.TypeParameter]: new TypeParameterRenderer(),
+  [ReflectionKind.Variable]: new PropertyRenderer(),
+}
